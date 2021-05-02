4459b254dc4f1c901af7ea75295b3019285a7891
kanzd <kanu0704@gmail.com>
2021-05-02 11:03:06 +0530

    02-05

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -1,11 +1,12 @@
 import React,{useState} from "react";
 
-import {MuiThemeProvider,Grid,List,LinearProgressWithLabel} from "@material-ui/core";
+import {MuiThemeProvider,Grid,List,LinearProgressWithLabel,Container} from "@material-ui/core";
 import {} from "@material-ui/icons";
 import Navbar from "../Shared/Navbar/index";
 import { createMuiTheme } from "@material-ui/core/styles";
 import UserArea from "./usercard/index";
 import testimage from "../../asset/user.png";
+import UserTable from "./usertable/index";
 const theme = createMuiTheme({
     palette: {
       primary: {
@@ -27,9 +28,13 @@ export default function Index(props)
     return (<>
     <MuiThemeProvider theme={theme}>
     <Navbar />
-    <Grid container >
+    <Grid container spacing={2}>
         <Grid item >
         <UserArea avatar={testimage} name={"Kanishk Dixit"} email={"kanu0704@gmail.com"} courseProgress={20} interviewProgress={30} />
+        </Grid>
+        <Grid item lg={9} sm={6} xm={1} style={{paddingTop:"2%"}}>
+        <UserTable  />
+        
         </Grid>
     </Grid>
     </MuiThemeProvider>
---
+++ src/components/User/usertable/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+import {Button} from "@material-ui/core";
+import {} from "@material-ui/icons";
+
+import { DataGrid } from '@material-ui/data-grid';
+
+const columns = [
+    { field: 'id', headerName: 'id', width: 70 },
+    { field: 'QUESNAME', headerName: 'QUES NAME', width: 180 },
+    { field: 'LASTATTEMPT', headerName: 'LAST ATTEMPT', width: 180 },
+    {
+      field: 'SUCCESSRATE',
+      headerName: 'SUCCESS RATE',
+      type: 'number',
+      width: 190,
+    },
+   {
+       field:"",
+       sortable: false,
+       width: 180,
+      
+       headerName:"ACTIONS",
+       renderCell:(props)=>{
+           return (<>
+               <Button  variant="contained" color={"primary"}>Click</Button>
+           </>)
+       }
+   }
+  ];
+  
+  const rows = [
+    { id: 1, QUESNAME: 'Snow', LASTATTEMPT: 'Jon', SUCCESSRATE: 35 },
+    { id: 2, QUESNAME: 'Snow', LASTATTEMPT: 'Jon', SUCCESSRATE: 35 },
+    { id: 3, QUESNAME: 'Snow', LASTATTEMPT: 'Jon', SUCCESSRATE: 35 },
+    { id: 4, QUESNAME: 'Snow', LASTATTEMPT: 'Jon', SUCCESSRATE: 35 },
+    { id: 5, QUESNAME: 'Snow', LASTATTEMPT: 'Jon', SUCCESSRATE: 35 },
+    { id: 6, QUESNAME: 'Snow', LASTATTEMPT: 'Jon', SUCCESSRATE: 35 },
+    { id: 7, QUESNAME: 'Snow', LASTATTEMPT: 'Jon', SUCCESSRATE: 35 },
+    { id: 8, QUESNAME: 'Snow', LASTATTEMPT: 'Jon', SUCCESSRATE: 35 },
+    { id: 9, QUESNAME: 'Snow', LASTATTEMPT: 'Jon', SUCCESSRATE: 35 },
+    { id: 10, QUESNAME: 'Snow', LASTATTEMPT: 'Jon', SUCCESSRATE: 35 },
+    { id: 11, QUESNAME: 'Snow', LASTATTEMPT: 'Jon', SUCCESSRATE: 35 },
+  ];
+export default function(props){
+
+    return (<>
+    <DataGrid rows={rows} columns={columns} pageSize={7}  />
+    </>)
+}
