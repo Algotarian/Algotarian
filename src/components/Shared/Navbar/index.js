bcde2348bfd637f02ff384b86ffcf0de891c7b7f
kanzd <kanu0704@gmail.com>
2021-05-29 08:19:58 +0530

    29-05-2021

--- src/components/Shared/Navbar/index.js
+++ src/components/Shared/Navbar/index.js
@@ -1,28 +1,66 @@
-import React from "react";
+import React,{useState} from "react";
 import {
     AppBar,
-  
+    ListItemAvatar,
+    
     Typography,
-
+    ListItemText,
     IconButton,
-
-   
+    List,
+    ListItem,
+   Drawer,
     Avatar,
     Toolbar,
     Container,
+    
 
   } from "@material-ui/core";
-  import { ListRounded,SupervisedUserCircleRounded } from "@material-ui/icons";
+  import { ListRounded,SupervisedUserCircleRounded,Home,Info,ContactSupport } from "@material-ui/icons";
   import CustomCard from "../Cards/index";
 import AlgotarianLogo from "../../../asset/Algotarian-Logo-white.png";
 
 export default function Index(props)
 {
-
+  const [state,setstate]=useState(false);
     return (<>
+    <Drawer anchor="left" open={state} onClose={(e)=>{
+      setstate(false);
+    }}>
+      <List>
+        <ListItem button>
+        <ListItemAvatar>
+        <Avatar>
+        <Home></Home>
+        </Avatar>
+       
+        </ListItemAvatar>
+          <ListItemText primary="Home" secondary="Get to the home page"></ListItemText>
+        </ListItem>
+        <ListItem button>
+        <ListItemAvatar>
+        <Avatar>
+        <Info></Info>
+        </Avatar>
+       
+        </ListItemAvatar>
+          <ListItemText primary="About Us" secondary="Information about Algotarian"></ListItemText>
+        </ListItem>
+        <ListItem button>
+        <ListItemAvatar>
+        <Avatar>
+        <ContactSupport></ContactSupport>
+        </Avatar>
+       
+        </ListItemAvatar>
+          <ListItemText primary="Support" secondary="Get Support"></ListItemText>
+        </ListItem>
+      </List>
+    </Drawer>
          <AppBar position="sticky">
           <Toolbar variant="dense">
-            <IconButton>
+            <IconButton onClick={(e)=>{
+              setstate(!state);
+            }}>
               <ListRounded style={{ color: "white",fontSize:"150%" }}></ListRounded>
             </IconButton>
             
